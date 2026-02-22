9c3663c0151b7122b50fb94091f35b717baf454f
Sudarshana Sadaruwan <sudarshana1999@gmail.com>
2026-02-22 17:38:40 +0530

    Reposition social links to the bottom of the footer for better tablet/mobile accessibility

--- components/Footer.js
+++ components/Footer.js
@@ -70,20 +70,7 @@ const Footer = () => {
                                 </li>
                             </ul>
 
-                            <div className="mt-8 flex gap-3">
-                                {/* Facebook */}
-                                <a href="#" className="flex h-8 w-8 items-center justify-center rounded bg-white text-brand-blue-deep hover:bg-brand-red-light hover:text-white transition-colors">
-                                    <svg className="h-4 w-4" fill="currentColor" viewBox="0 0 24 24"><path d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" /></svg>
-                                </a>
-                                {/* LinkedIn */}
-                                <a href="#" className="flex h-8 w-8 items-center justify-center rounded bg-white text-brand-blue-deep hover:bg-brand-red-light hover:text-white transition-colors">
-                                    <svg className="h-4 w-4" fill="currentColor" viewBox="0 0 24 24"><path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" /></svg>
-                                </a>
-                                {/* YouTube */}
-                                <a href="#" className="flex h-8 w-8 items-center justify-center rounded bg-white text-brand-red-light hover:bg-brand-red-light hover:text-white transition-colors">
-                                    <svg className="h-4 w-4" fill="currentColor" viewBox="0 0 24 24"><path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z" /></svg>
-                                </a>
-                            </div>
+
                         </div>
 
                         {/* Explore (Sri Lanka) */}
@@ -192,8 +179,22 @@ const Footer = () => {
                         </div>
                     </div>
 
-                    {/* Bottom Copyright */}
-                    <div className="border-t border-white/20 pt-6 text-center">
+                    {/* Social Links & Copyright */}
+                    <div className="border-t border-white/20 pt-8 flex flex-col items-center gap-6">
+                        <div className="flex gap-4">
+                            {/* Facebook */}
+                            <a href="#" className="flex h-10 w-10 items-center justify-center rounded bg-white text-brand-blue-deep hover:bg-brand-red-light hover:text-white transition-all transform hover:scale-110">
+                                <svg className="h-5 w-5" fill="currentColor" viewBox="0 0 24 24"><path d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" /></svg>
+                            </a>
+                            {/* LinkedIn */}
+                            <a href="#" className="flex h-10 w-10 items-center justify-center rounded bg-white text-brand-blue-deep hover:bg-brand-red-light hover:text-white transition-all transform hover:scale-110">
+                                <svg className="h-5 w-5" fill="currentColor" viewBox="0 0 24 24"><path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" /></svg>
+                            </a>
+                            {/* YouTube */}
+                            <a href="#" className="flex h-10 w-10 items-center justify-center rounded bg-white text-brand-red-light hover:bg-brand-red-light hover:text-white transition-all transform hover:scale-110">
+                                <svg className="h-5 w-5" fill="currentColor" viewBox="0 0 24 24"><path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z" /></svg>
+                            </a>
+                        </div>
                         <p className="text-sm font-medium text-white/90">
                             Copyright © 2026 Celata tech (Pvt) Ltd | All Rights Reserved
                         </p>
