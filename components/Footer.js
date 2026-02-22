0acca09a3f67c9adf95c940a350e4b879468e96f
Sudarshana Sadaruwan <sudarshana1999@gmail.com>
2026-02-22 17:42:37 +0530

    Align footer social links and copyright side-by-side in tablet and desktop views

--- components/Footer.js
+++ components/Footer.js
@@ -180,8 +180,11 @@ const Footer = () => {
                     </div>
 
                     {/* Social Links & Copyright */}
-                    <div className="border-t border-white/20 pt-8 flex flex-col items-center gap-6">
-                        <div className="flex gap-4">
+                    <div className="border-t border-white/20 pt-8 flex flex-col md:flex-row justify-between items-center gap-6">
+                        <p className="text-sm font-medium text-white/90 order-2 md:order-1">
+                            Copyright © 2026 Celata tech (Pvt) Ltd | All Rights Reserved
+                        </p>
+                        <div className="flex gap-4 order-1 md:order-2">
                             {/* Facebook */}
                             <a href="#" className="flex h-10 w-10 items-center justify-center rounded bg-white text-brand-blue-deep hover:bg-brand-red-light hover:text-white transition-all transform hover:scale-110">
                                 <svg className="h-5 w-5" fill="currentColor" viewBox="0 0 24 24"><path d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" /></svg>
@@ -195,9 +198,6 @@ const Footer = () => {
                                 <svg className="h-5 w-5" fill="currentColor" viewBox="0 0 24 24"><path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z" /></svg>
                             </a>
                         </div>
-                        <p className="text-sm font-medium text-white/90">
-                            Copyright © 2026 Celata tech (Pvt) Ltd | All Rights Reserved
-                        </p>
                     </div>
                 </div>
             </div>
